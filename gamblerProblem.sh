f8f03e90716412c61c2abffdc0c3b900779f1e93
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-08 17:51:37 +0530

    UC 4 added

--- gamblerProblem.sh
+++ gamblerProblem.sh
@@ -13,27 +13,50 @@ fi
 echo "Won : $won$"
 echo "Lost : $lost$"
 
-#UC 3 logic
+#UC 3 logic updated to UC 4
 echo
-echo "UC 3"
-declare -a arr
+declare -a day
 upper=150
 lower=50
 ammount=0
-dummy_stake=$stake
-while [[ $dummy_stake -gt $lower && $dummy_stake -lt $upper  ]]
+
+days=20
+for (( i=0; i<$days; i++))
 do
-        bet=$(( RANDOM % 2 ))
-        if [[ $bet -eq 1 ]]
-        then
+	dummy_stake=$stake
+	while [[ $dummy_stake -gt $lower && $dummy_stake -lt $upper  ]]
+	do
+        	bet=$(( RANDOM % 2 ))
+        	if [[ $bet -eq 1 ]]
+        	then
                 	let "dummy_stake+=1"
-	else
+		else
         	        let "dummy_stake-=1"
-       	fi
+       		fi
+	done
+	let "ammount+=$(($stake-$dummy_stake))"
+	day[$i]=$(($ammount))
 done
-if [[ $dummy_stake -eq "150" ]]
-then
-	echo "Won : $(($dummy_stake - $stake))$"
-else
-	echo "Lost : $dummy_stake$"
-fi
+i=0;
+value=0
+for elements in ${day[@]}
+do
+	let "i+=1"
+	if [[ $elements -gt $value ]]
+	then
+		echo "Won on day $i : $elements$"
+	else
+		echo "Lost on day $i : $elements"
+	fi
+	value=${day[$i-1]}
+done
+
+echo "Total amount left with the gambler = ${day[$i-1]}"
+
+#if [[ $dummy_stake -eq "150" ]]
+#then
+#	echo "Won : $(($dummy_stake - $stake))$"
+#else
+#	echo "Lost : $dummy_stake$"
+#fi
+
