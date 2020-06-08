bf71562472750469e410ff23bb04c3b45ccbe05e
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-08 18:15:34 +0530

    UC 5 completed

--- gamblerProblem.sh
+++ gamblerProblem.sh
@@ -3,6 +3,7 @@ stake=100
 gambler_stake=100
 won=0
 lost=0
+default=0
 bet=$((RANDOM % 2))
 if [[ $bet -eq 1 ]]
 then
@@ -13,15 +14,17 @@ fi
 echo "Won : $won$"
 echo "Lost : $lost$"
 
-#UC 3 logic updated to UC 4
+#UC 3 logic updated to UC 4 updated to UC 5
 echo
 declare -a day
 upper=150
 lower=50
 ammount=0
-
+month=30
 days=20
-for (( i=0; i<$days; i++))
+daysWon=0
+daysLost=0
+for (( i=0; i<$month; i++))
 do
 	dummy_stake=$stake
 	while [[ $dummy_stake -gt $lower && $dummy_stake -lt $upper  ]]
@@ -45,11 +48,20 @@ do
 	if [[ $elements -gt $value ]]
 	then
 		echo "Won on day $i : $elements$"
+		let "daysWon+=1"
 	else
 		echo "Lost on day $i : $elements"
+		let "daysLost+=1"
 	fi
 	value=${day[$i-1]}
 done
 
+echo
 echo "Total amount left with the gambler = ${day[$i-1]}"
-
+echo "Total days gamber won is $daysWon and lost is $daysLost"
+if [[ $ammount -lt $default ]]
+then
+	echo "Lost by $ammount$"
+else
+	echo "Won by $ammount$"
+fi
