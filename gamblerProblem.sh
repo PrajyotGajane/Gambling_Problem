cee40aa972dd20a9e42c4f24c803e2981e9f9b53
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-08 17:05:29 +0530

    UC 1 completed

--- gamblerProblem.sh
+++ gamblerProblem.sh
@@ -1,1 +1,14 @@
 echo "Welcome to the gambling simultation program"
+stake=100
+gambler_stake=100
+won=0
+lost=0
+bet=$((RANDOM % 2))
+if [[ $bet -eq 1 ]]
+then
+    let "won+=1"
+else
+    let "lost+=1"
+fi
+echo "Won : $won$"
+echo "Lost : $lost$"
