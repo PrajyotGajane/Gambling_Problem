72dcde9b50109c0c069ff5d78aab47c2714a4f61
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-08 17:30:20 +0530

    UC 3 completed

--- gamblerProblem.sh
+++ gamblerProblem.sh
@@ -12,3 +12,28 @@ else
 fi
 echo "Won : $won$"
 echo "Lost : $lost$"
+
+#UC 3 logic
+echo
+echo "UC 3"
+declare -a arr
+upper=150
+lower=50
+ammount=0
+dummy_stake=$stake
+while [[ $dummy_stake -gt $lower && $dummy_stake -lt $upper  ]]
+do
+        bet=$(( RANDOM % 2 ))
+        if [[ $bet -eq 1 ]]
+        then
+                	let "dummy_stake+=1"
+	else
+        	        let "dummy_stake-=1"
+       	fi
+done
+if [[ $dummy_stake -eq "150" ]]
+then
+	echo "Won : $(($dummy_stake - $stake))$"
+else
+	echo "Lost : $dummy_stake$"
+fi
